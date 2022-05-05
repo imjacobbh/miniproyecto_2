e5570e5107f86a53f2d6d3d1211f1e4af8978d53
jacobFinloop <jacob.bonilla@finloop.com.mx>
2022-05-04 21:23:30 -0500

    fixing search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import { HeroesComponent } from './heroes/heroes.component'
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { UnheroeComponent } from './unheroe/unheroe.component';
@@ -10,10 +10,10 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'heroes', component: HeroesComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home'},
 
-  {path: 'hereo/:id', component: UnheroeComponent},
-  {path: 'buscador/:nombreh', component: SearchComponent}
+  {path: 'heroe/:id', component: UnheroeComponent},
+  {path: 'buscador/:nombreH', component: SearchComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
