8568670a38e34d6fe87399215dcada38178a0064
jacobFinloop <jacob.bonilla@finloop.com.mx>
2022-06-01 11:36:11 -0500

    adding pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { SuscriptoresComponent } from './suscriptores/suscriptores.component';
 import { VideoComponent } from './video/video.component';
 import { DomseguroPipe } from './domseguro.pipe';
 import { ContactoComponent } from './contacto/contacto.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -33,7 +35,13 @@ import { ContactoComponent } from './contacto/contacto.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule 
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }) 
   ],
   providers: [],
   bootstrap: [AppComponent]
